0dab6df3f7a6b09c6538b9795ea10b90e6ae2838
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-02-05 16:39:32 +0530

    Time: 3 ms (64.64%), Space: 7.8 MB (36.17%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0;
+        int b=1;
+        int c;
+        if(n==0){
+            return 0;
+        }
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
