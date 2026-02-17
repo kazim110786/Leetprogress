74c1c001ac8661dcd89391ba285760a827036fdd
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-02-17 23:01:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-     stack<char> st;
-     for(char ch:s){
-        if(ch=='(' || ch=='{' || ch=='['){
-            st.push(ch);
-        }else{
-            if(st.empty()){
-                return false;
-            }
-            if(ch==')' && st.top()=='('){
-                st.pop();
-            }else if(ch=='}' && st.top()=='{'){
-                st.pop();
-            }else if(ch==']' && st.top()=='['){
-                st.pop();
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
             }else{
-                return false;
+                if(st.size()==0){
+                    return false;
+                }
+                char ch=st.top();
+                st.pop();
+                if(s[i]==')' && ch!='(' ||
+                s[i]==']' && ch!='[' ||
+                s[i]=='}' && ch!='{'){
+                    return false;
+                }
             }
+            
         }
-    }
-    return st.empty();
-    }
+    
+        if(st.size()==0){
+            return true;
+        }else{
+            return false;
+        }
+}
 };
