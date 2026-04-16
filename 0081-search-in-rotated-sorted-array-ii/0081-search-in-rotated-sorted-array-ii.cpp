426b95f4bf8f55cfc981930f65de302a6c584778
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-04-16 09:56:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,11 +8,26 @@ public:
             int mid=(low+high)/2;
             if(nums[mid]==target){
                 return true;
-            }else if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
                 low+=1;
                 mid-=1;
                 continue;
-            }else if(nums[low]<target && nums[mid]>target)
+            }
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
         }
+        return false;
     }
 };
