0d228a22af401d427d6cc6b643695064d06032ca
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-04-20 22:39:48 +0530

    Time: 4 ms (5.67%), Space: 13.9 MB (46.07%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        int index=0;
+        int i=0;
+        while(i<n){
+            char curr = chars[i];
+            int count=0;
+            while(i<n && chars[i]==curr){
+                i++;
+                count++;
+            }
+            chars[index++]=curr;
+            if(count>1){
+                string cnt=to_string(count);
+                for(int j=0;j<cnt.length();j++){
+                    chars[index++]=cnt[j];
+                }
+            }
+        }
+        return index;
+    }
+};
