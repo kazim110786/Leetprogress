10c15a749275fecc24d149fb07a4c1c69596b919
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-04-28 22:29:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,15 +6,12 @@ public:
         sort(strs.begin(),strs.end());
         string first=strs[0];
         string last=strs[n-1];
-        for(int i=0;i<first.size();i++){
+        for(int i=0;i<n;i++){
             if(first[i]==last[i]){
                 ans.push_back(first[i]);
             }else{
                 break;
             }
-        }
-        if(ans.length()==0){
-            return "";
         }
         return ans;
     }
