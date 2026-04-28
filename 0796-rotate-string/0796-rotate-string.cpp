8fc319fb6de8a20435e3ee8d1da62eeccf8b8cd3
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-04-28 23:07:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        string ans=s+s;
+        if(ans.find(goal)!=-1){
+            return true;
+        }
+        return false;
+    }
+};
