a685427e4aa5ddd3dc74f2024c6bdb6777d13235
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-05-07 10:54:09 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         int maxi=INT_MIN;
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
-            maxi=max(maxi,sum);
+            maxi=max(sum,maxi);
             if(sum<0){
                 sum=0;
             }
