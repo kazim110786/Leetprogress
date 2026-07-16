61009c6ec676b2badba3d4be9d79f06d2ea804d9
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-07-16 22:21:42 +0530

    Time: 0 ms (100%), Space: 11.2 MB (82.34%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,15 +4,13 @@ public:
         int n=nums.size();
         int count=0;
         for(int i=0;i<n;i++){
-            if(nums[i]>nums[(i+1) % n]){
+            if(nums[i]>nums[(i+1)%n]){
                 count++;
             }
         }
         if(count<=1){
             return true;
-        }else{
-            return false;
         }
-
+        return false;
     }
 };
