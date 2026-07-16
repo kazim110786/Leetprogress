2512e084690c838e28cf785005ea62c220ca04d4
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-07-16 09:40:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.9%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string build(string r){
+        string ans="";
+        for(char c:r){
+            if(c!='#'){
+                ans.push_back(c);
+            }else if(!ans.empty()){
+                ans.pop_back();
+            }
+        }
+        return ans;
+    }
+    bool backspaceCompare(string s, string t) {
+        return build(s)==build(t);
+    }
+};
