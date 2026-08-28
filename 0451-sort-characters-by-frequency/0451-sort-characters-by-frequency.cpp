613c6d3ebc04c2eca01b21bfeee6847c3939faef
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-08-28 22:34:51 +0530

    Time: 12 ms (6.08%), Space: 10.3 MB (97.71%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string frequencySort(string s) {
         int freq[256]={0};
-        for(char a:s){
-            freq[a]++;
+        for(char ch:s){
+            freq[ch]++;
         }
         sort(s.begin(),s.end(),[&](char a,char b){
             if(freq[a]==freq[b]){
-                return b>a;
+                return a>b;
             }
             return freq[a]>freq[b];
         });
