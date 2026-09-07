bf05c614805834da78917e0f522b467907522623
kazim110786 <143737336+kazim110786@users.noreply.github.com>
2026-09-07 11:37:29 +0530

    Time: 8 ms (50.39%), Space: 13.6 MB (83.14%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.length();
         string str=s+s;
         int pos=str.find(s,1);
-        if(pos>0 && pos<n){
+        if(pos>=0 && pos<n){
             return true;
         }
         return false;
